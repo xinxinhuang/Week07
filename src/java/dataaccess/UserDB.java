2ead538f3b22a12506d938ee96d3ff4c8432257a
728618 <728618@ELF6215898.ACDM.DS.SAIT.CA>
2017-10-20 09:12:58 -0600

    UserDB method fixes fixes for getall and getuser

--- src/java/dataaccess/UserDB.java
+++ src/java/dataaccess/UserDB.java
@@ -1,6 +1,7 @@
 package dataaccess;
 
 import domainmodel.User;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -23,7 +24,7 @@ public class UserDB {
             ResultSet rs = ps.executeQuery();
             List<User> users = new ArrayList<>();
             while(rs.next()){
-                users.add(new User());
+                users.add(new User(rs.getString("username"), rs.getString("password"), rs.getString("email"), rs.getInt("active"), rs.getString("firstname"), rs.getString("lastname")));
             }
             return users;
         } catch (SQLException ex) {
@@ -41,8 +42,8 @@ public class UserDB {
         ConnectionPool pool = ConnectionPool.getInstance();
         Connection connection = pool.getConnection();
         String selectSQL = "SELECT * FROM User WHERE username = ?";
-        PreparedStatement ps = connection.prepareStatement(selectSQL);
         try {
+            PreparedStatement ps = connection.prepareStatement(selectSQL);
             ps.setString(1, username);
             ResultSet rs = ps.executeQuery();
        
